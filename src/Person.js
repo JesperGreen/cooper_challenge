2fc0049c0cae3d7db145a8cf7ffcce511b20a4dd
Jesper <jesper_gs_@hotmail.com>
2016-09-21 08:30:33 +0200

    added function to get results depending on gender

--- src/Person.js
+++ src/Person.js
@@ -1,4 +1,12 @@
 function Person(attr) {
   this.gender = attr.gender;
   this.age = attr.age;
+
+  Person.prototype.cooperDistance = function (distance) {
+    if (this.gender.toUpperCase() = 'MALE') {
+      this.cooperMessage = this.getCooperResultsForMales(distance);
+    } else {
+      this.cooperMessage = this.getCooperResultsforFemales(distance);
+    }
+  };
 };
