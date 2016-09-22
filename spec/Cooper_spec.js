e075a80247575533d264e26e758fe365d087c859
Jesper <jesper_gs_@hotmail.com>
2016-09-22 05:46:21 +0200

    added 2 tests to the Cooper_spec file

--- spec/Cooper_spec.js
+++ spec/Cooper_spec.js
@@ -1,3 +1,19 @@
 describe("Person", function() {
-  
-}
+
+  beforeEach(function) {
+    person = new Person({
+      gender: 'Male',
+      age: 23
+    });
+  };
+
+  it("Should get a Poor result", function(){
+    person.cooperDistance(1500);
+    expect(person.cooperMessage).toBe('Poor')
+  })
+
+  it("Should get a Excellent result", function(){
+    person.cooperDistance(5000);
+    expect(person.cooperMessage).toBe('Excellent')
+  })
+});
