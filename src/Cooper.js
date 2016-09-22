f6d40ff7c92fbc9300ee7086f771a5d189dd0dc9
Jesper <jesper_gs_@hotmail.com>
2016-09-22 04:31:53 +0200

    added results for age 13-14 to the case method

--- src/Cooper.js
+++ src/Cooper.js
@@ -15,8 +15,18 @@ function Person(attr) {
     switch (true) {
       case (age.between(13,14)):
         switch (true) {
-          
+          case (distance <= 2099):
+            return 'Poor';
+          case (distance.between(2100, 2199)):
+            return 'Below average';
+          case (distance.between(2200, 2399)):
+            return 'Average';
+          case (distance.between(2400, 2699)):
+            return 'Above average';
+          case (distance >= 2700):
+            return 'Excellent';
         }
+        break;
     }
   }
 };
